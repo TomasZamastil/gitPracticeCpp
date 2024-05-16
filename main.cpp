66d76a5be999b1824d0728209784b65843c3857e
Toxicyenom <tysonyt10@gmail.com>
2024-05-16 21:21:44 +0530

    Little changes
    1)I have added some new lines and syntaxes.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
-
+#include <vector>
+#include <string>
 int main(){
+  typedef std::string st;
+  st name ="Hello!";
+  std::cout << name << "\n";
   std::cout << "First file";
   return 0;
 }
